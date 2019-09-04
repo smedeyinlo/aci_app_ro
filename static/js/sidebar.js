54fb5ae6ca528ee660e6df2ca68ed23e03b6a986
smedeyinlo <segunmedeyinlo@gmail.com>
2019-09-04 16:06:31 +0100

    initial commit

---
+++ static/js/sidebar.js
@@ -0,0 +1,29 @@
+/*$(id="#menu_item").mouseover(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+*/
+/*$(id="#sidebar-wrapper").mouseenter(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+
+$(id="#sidebar-wrapper").mouseleave(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+$(id="#sidebar-wrapper-scroll").mouseenter(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+
+$(id="#sidebar-wrapper-scroll").mouseleave(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+
+/* 
+$(id="#sub_icon").mouseover(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+
+$(id="#menu-toggle").click(function(e) {
+        $("#wrapper").toggleClass("toggled");
+});
+
+*/
